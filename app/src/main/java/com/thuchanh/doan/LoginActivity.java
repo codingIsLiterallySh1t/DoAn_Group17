408d0bafa1e7737b84391f63c7348863ceaa643a
Anti_coding <nguyenvoquocbao2805@gmail.com>
2024-03-04 09:56:47 +0700

    N1gga Interface

---
+++ app/src/main/java/com/thuchanh/doan/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.thuchanh.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edtID, edtPass;
+    Button btnLogIn,btnReg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edtID = findViewById(R.id.editEmail);
+        edtPass = findViewById(R.id.editPassword);
+        btnLogIn=findViewById(R.id.LoginBtn);
+        btnReg=findViewById(R.id.SignUpBtn);
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                Bundle b = new Bundle();
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                Bundle b = new Bundle();
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/thuchanh/doan/MainActivity.java
@@ -0,0 +1,60 @@
+package com.thuchanh.doan;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    InfoFragment infoFragment = new InfoFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, homeFragment)
+                        .commit();
+                return true;
+
+            case R.id.info:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, infoFragment)
+                        .commit();
+                return true;
+
+            case R.id.settings:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, settingsFragment)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+}
