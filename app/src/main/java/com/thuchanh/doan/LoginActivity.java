780e5e0aeba40961425784f4995fe0dbd244603a
dzankhanh <khanhnguyen0897@gmail.com>
2024-03-10 02:55:28 +0700

    update

--- app/src/main/java/com/thuchanh/doan/LoginActivity.java
+++ app/src/main/java/com/thuchanh/doan/LoginActivity.java
@@ -1,16 +1,28 @@
 package com.thuchanh.doan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class LoginActivity extends AppCompatActivity {
     EditText edtID, edtPass;
     Button btnLogIn,btnReg;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,23 +31,51 @@ public class LoginActivity extends AppCompatActivity {
         edtPass = findViewById(R.id.edtPassword);
         btnLogIn=findViewById(R.id.LoginBtn);
         btnReg=findViewById(R.id.SignUpBtn);
+        mAuth=FirebaseAuth.getInstance();
+
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                Bundle b = new Bundle();
-                intent.putExtras(b);
-                startActivity(intent);
+                login();
             }
         });
+
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                Bundle b = new Bundle();
-                intent.putExtras(b);
-                startActivity(intent);
+               register();
+            }
+        });
+    }
+
+    private void register(){
+        Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(i);
+    }
+    private void login(){
+        String email, pass;
+        email = edtID.getText().toString();
+        pass = edtPass.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Vui lòng nhập Email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass)){
+            Toast.makeText(this,"Vui lòng nhập password!!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Đăng nhập không thành công!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/thuchanh/doan/RegisterActivity.java
+++ app/src/main/java/com/thuchanh/doan/RegisterActivity.java
@@ -1,14 +1,70 @@
 package com.thuchanh.doan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edtID, edtPass;
+    Button btnReg;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
+        edtID = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPassword);
+        btnReg = findViewById(R.id.SignUpBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        String email, pass;
+        email = edtID.getText().toString();
+        pass = edtPass.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Vui lòng nhập Email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Vui lòng nhập password!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Tạo tài khoản không thành công", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
