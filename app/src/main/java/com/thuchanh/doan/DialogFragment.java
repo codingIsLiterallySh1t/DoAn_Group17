3d23fe89b2952969d9b3c75d3988a9e1d21fad86
DzanKhanh <khanhnguyen0897@gmail.com>
2024-03-23 20:53:32 +0700

    update info

--- app/src/main/java/com/thuchanh/doan/DialogFragment.java
+++ app/src/main/java/com/thuchanh/doan/DialogFragment.java
@@ -10,10 +10,11 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link InfoFragment#newInstance} factory method to
+ * Use the {@link DialogFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class InfoFragment extends Fragment {
+public class DialogFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +25,7 @@ public class InfoFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public InfoFragment() {
+    public DialogFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +35,11 @@ public class InfoFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment InfoFragment.
+     * @return A new instance of fragment DialogFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static InfoFragment newInstance(String param1, String param2) {
-        InfoFragment fragment = new InfoFragment();
+    public static DialogFragment newInstance(String param1, String param2) {
+        DialogFragment fragment = new DialogFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,12 +54,13 @@ public class InfoFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
     }
 }
