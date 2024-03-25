8636599bc86c563a76edc7be8e59432705b2fefb
Bùi Gia Bảo <22dh110224@st.huflit.edu.vn>
2024-03-25 09:14:33 +0700

    trang chi tiet trang

--- app/src/main/java/com/thuchanh/doan/MainActivity_Guest.java
+++ app/src/main/java/com/thuchanh/doan/MainActivity_Guest.java
@@ -2,6 +2,8 @@ package com.thuchanh.doan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity_Guest extends AppCompatActivity {
 
+    ImageButton img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,6 +38,16 @@ public class MainActivity_Guest extends AppCompatActivity {
                     return true;
             }
             return false;
+        });
+
+        img1 = (ImageButton) findViewById(R.id.btnIMG1);
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity_Guest.this, DetailActivity_Guest.class);
+                startActivity(intent);
+            }
         });
     }
 }
